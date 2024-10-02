8b0f7dc9c55614a397fff1f32d8203364922bc0c
Caio Rocha <caiobispo1@gmail.com>
2024-10-02 10:59:13 -0300

    teste object

--- helloWorld.js
+++ helloWorld.js
@@ -1,21 +1,2 @@
-(function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-      define([], factory);
-    } else if (typeof exports !== "undefined") {
-      factory();
-    } else {
-      var mod = {
-        exports: {}
-      };
-      factory();
-      global.repl = mod.exports;
-    }
-  })(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function () {
-    "use strict";
-  
-    const MyLibrary = {};
-    MyLibrary.testMessage = () => {
-      console.log("Hello World");
-    };
-    }
-);
+const MyLibrary = {'message': 'Hello World'}
+
